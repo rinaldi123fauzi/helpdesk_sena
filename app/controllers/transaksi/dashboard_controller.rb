192b10bb336e42bb9b34e8320b9c0d213e7f46f6
Mohamad Rinaldi Fauzi <rinalfauzi@gmail.com>
2023-06-25 01:51:03 +0700

    perbaikan query get data buat grafik dashboard v2

--- app/controllers/transaksi/dashboard_controller.rb
+++ app/controllers/transaksi/dashboard_controller.rb
@@ -7,91 +7,12 @@ class Transaksi::DashboardController < ApplicationController
     #   @tahun = Time.now.strftime('%Y')
     # end
 
-    # open
     array_open = []
-    for a in 1..12 do
-      if a <= 9
-        bulan = "0"+a.to_s
-      else
-        bulan = a.to_s
-      end
-      @open = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'open', @tahun, bulan).count
-      array_open.push(
-        'amount' => @open
-      )
-    end
-
-    # approval
     array_approval = []
-    for a in 1..12 do
-      if a <= 9
-        bulan = "0"+a.to_s
-      else
-        bulan = a.to_s
-      end
-      @approval = Ticket.where('status IN (?) and extract(year from created_at) = ? and extract(month from created_at) = ?', ['approval1','approval2','approval3'], @tahun, bulan).count
-      array_approval.push(
-        'amount' => @approval
-      )
-    end
-
-    # overdue
     array_overdue = []
-    for a in 1..12 do
-      if a <= 9
-        bulan = "0"+a.to_s
-      else
-        bulan = a.to_s
-      end
-      @overdue = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'overdue', @tahun, bulan).count
-      array_overdue.push(
-        'amount' => @overdue
-      )
-    end
-
-    # inprogress
     array_inprogress = []
-    for a in 1..12 do
-      if a <= 9
-        bulan = "0"+a.to_s
-      else
-        bulan = a.to_s
-      end
-      @inprogress = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'inprogress', @tahun, bulan).count
-      array_inprogress.push(
-        'amount' => @inprogress
-      )
-    end
-
-    # created
     array_created = []
-    for a in 1..12 do
-      if a <= 9
-        bulan = "0"+a.to_s
-      else
-        bulan = a.to_s
-      end
-      @created = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'created', @tahun, bulan).count
-      array_created.push(
-        'amount' => @created
-      )
-    end
-
-    # closed
     array_closed = []
-    for a in 1..12 do
-      if a <= 9
-        bulan = "0"+a.to_s
-      else
-        bulan = a.to_s
-      end
-      @closed = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'closed', @tahun, bulan).count
-      array_closed.push(
-        'amount' => @closed
-      )
-    end
-
-    # rejected
     array_rejected = []
     for a in 1..12 do
       if a <= 9
@@ -99,6 +20,44 @@ class Transaksi::DashboardController < ApplicationController
       else
         bulan = a.to_s
       end
+
+      # open
+      @open = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'open', @tahun, bulan).count
+      array_open.push(
+        'amount' => @open
+      )
+
+      # approval
+      @approval = Ticket.where('status IN (?) and extract(year from created_at) = ? and extract(month from created_at) = ?', ['approval1','approval2','approval3'], @tahun, bulan).count
+      array_approval.push(
+        'amount' => @approval
+      )
+
+      # overdue
+      @overdue = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'overdue', @tahun, bulan).count
+      array_overdue.push(
+        'amount' => @overdue
+      )
+
+      # inprogress
+      @inprogress = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'inprogress', @tahun, bulan).count
+      array_inprogress.push(
+        'amount' => @inprogress
+      )
+
+      # created
+      @created = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'created', @tahun, bulan).count
+      array_created.push(
+        'amount' => @created
+      )
+
+      # closed
+      @closed = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'closed', @tahun, bulan).count
+      array_closed.push(
+        'amount' => @closed
+      )
+
+      # rejected
       @rejected = Ticket.where('status = ? and extract(year from created_at) = ? and extract(month from created_at) = ?', 'rejected', @tahun, bulan).count
       array_rejected.push(
         'amount' => @rejected
