514d43de043acc7d3e221cb3dba164dc55a2421b
Mohamad Rinaldi Fauzi <rinalfauzi@gmail.com>
2023-08-20 17:07:29 +0700

    add delete button di halaman sender_emails

--- app/models/sender_email.rb
+++ app/models/sender_email.rb
@@ -11,17 +11,4 @@
 #  updated_at :datetime         not null
 #
 class SenderEmail < ApplicationRecord
-  def send
-    checkSender = SenderEmail.where(status: 'not-yet-sent')
-    checkSender.each do |data|
-      tickets = Ticket.where(id: data.parent_id)
-      if tickets.count == 1
-        ticket = tickets.first
-        UserMailer.approval_confirmation(data.email_to,ticket,data.token).deliver_now
-      end
-      sender_email = SenderEmail.find_by(id: data.id)
-      sender_email.status = "sent"
-      sender_email.save
-    end
-  end
 end
